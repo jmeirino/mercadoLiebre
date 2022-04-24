334286fea5f2b44bb783e73ecd9d768315150fe0
jmeirino <juanchomeirino@gmail.com>
2022-04-24 11:31:18 -0300

    app

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const port = 3030;
 
 console.log(__dirname);
-app.use('/static', express.static(__dirname + 'public'));
+app.use('/static', express.static(__dirname + '/public'));
 
 app.listen(process.env.PORT || port , ()=>{
     console.log('Servidor Corriendo');
